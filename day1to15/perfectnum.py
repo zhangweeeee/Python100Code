8e37ee7b1e6259748d1f19a434ac8d7354efe5c1
zhangweeeee <zhangweeeee@163.com>
2019-11-07 21:39:14 +0800

    day5 完美数

--- day1to15/perfectnum.py
+++ day1to15/perfectnum.py
@@ -2,7 +2,7 @@
 #   说明：完美数又称为完全数或完备数，它的所有的真因子（即除了自身以外的因子）的和（即因子函数）恰好等于它本身。
 #   例如：6（$6=1+2+3$）和28（$28=1+2+4+7+14$）就是完美数。完美数有很多神奇的特性，有兴趣的可以自行了解。
 import math
-for number in range(1,10000000000000):
+for number in range(1,10000):
     sum = 0
     for factory in range(1,number):
         if number % factory == 0:
