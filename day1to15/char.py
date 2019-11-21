eb47b0bbbd94b45c292b2afdbfc7b1acab58b9c5
zhangweeeee <zhangweeeee@163.com>
2019-11-21 12:04:49 +0800

    数据库

---
+++ day1to15/char.py
@@ -0,0 +1,17 @@
+import sys
+print(ord('A'))
+print(ord('a'))
+print(ord('中'))
+print('\u4e2d\u6587')
+
+x = b'ABCd'
+y = 'ABCd'
+
+print(len(x))
+print(len(y))
+
+p = ['asp','php']
+s = ['java','c++',p,'scheme']
+
+print(p[1])
+print(s[2][0])
