133e0f427376c9281dd213d651f50381fd0a3fb8
zhangweeeee <zhangweeeee@163.com>
2019-12-16 11:10:30 +0800

    列表生成式

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python100Code
 学习python100days代码实现
+以及阅读书籍《python编程：从入门到实践》代码实现
---
+++ day1to15/python从入门到实践-实现/hello_world.py
@@ -0,0 +1,13 @@
+#--------------------------------------
+# Coding:   utf-8
+# Author:   zhangwei
+# Time:     2019/12/13 15:17
+# Filename: hello_world
+# Notes:
+#--------------------------------------
+message = 'hello world'
+print(message)
+print(message.title())
+print(message.upper())
+print(message.lower())
+print(message)
