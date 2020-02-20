5ddaafef5454efb9bc5a9c66fdb6de765049b5d7
zhangweeeee <zhangweeeee@163.com>
2020-02-20 12:38:52 +0800

    测试机械键盘

---
+++ python从入门到实践/text.py
@@ -0,0 +1,11 @@
+#-----------------------------
+# Coding:   utf-8
+# Author:   zhangwei
+# Time:     2020/2/2012:35
+# Filename: text
+# Notes: 测试机械键盘，哈哈哈
+#-----------------------------
+a = int(input())
+b = int(input())
+print(a+b)
+
