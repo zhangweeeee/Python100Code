922fc7f1c539faf127adeda60dc7c0503bfc2bdd
zhangweeeee <zhangweeeee@163.com>
2020-08-18 23:25:32 +0800

    测试机械键盘

---
+++ python从入门到实践/magicians.py
@@ -0,0 +1,11 @@
+#-----------------------------
+# Coding:   utf-8
+# Author:   zhangwei
+# Time:     2020/2/2110:54
+# Filename: magicians
+# Notes:列表遍历
+#-----------------------------
+magicians =['alice','david','carolina']
+for magician in magicians:
+    print(magician.title()+", that is great")
+
